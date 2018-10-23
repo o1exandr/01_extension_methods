4f00b806ffa26a0e0e496c25c3118b78fc356fe2
Oleksandr Finiuk <olexandr.finyuk@gmail.com>
2018-10-23 21:23:02 +0300

    Add project files.

---
+++ 01_extension_methods/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_extension_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
